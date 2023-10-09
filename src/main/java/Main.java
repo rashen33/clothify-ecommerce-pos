2cd4cffb1e967c84702e25d47a9fab2934fe0966
Rashen <rashen598@gmail.com>
2023-10-09 10:21:22 +0530

    initialized the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Arrays;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/loginPage.fxml"))));
+        stage.show();
+    }
+}
