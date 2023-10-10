4f6aef93f249b0d6e2a14ee1e56106b93e0f9c4e
Rashen <rashen598@gmail.com>
2023-10-10 14:49:16 +0530

    added dashboard, dashboard root views

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+f
+6src/main/java/edu/icet/controller/LoginController.java,4\0\4004bcd2a8f092db0b55bc52b258cea42b3bb944
+G
+src/main/java/Main.java,e\4\e44a8606ce010ad9b89aafdfcee2ab49afbf6d10
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/loginPage.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dashboardRoot.fxml"))));
         stage.show();
     }
 }
