1d73b2399681c251cf632344c44b6f740333cb68
Rashen <rashen598@gmail.com>
2023-10-14 22:32:31 +0530

    added clear field method

--- src/main/java/edu/icet/controller/ItemController.java
+++ src/main/java/edu/icet/controller/ItemController.java
@@ -27,9 +27,7 @@ public class ItemController implements Initializable {
     public TextField txtSellPrice;
     public ComboBox cmbType;
     public ComboBox cmbSize;
-    public TextField txtProfit;
     public ComboBox cmbSupId;
-    public ComboBox cmbSupName;
     public TreeTableColumn colCod;
     public TreeTableColumn colD;
     public TreeTableColumn colQty;
@@ -44,56 +42,10 @@ public class ItemController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        generateId();
         loadCmbSupId();
-        loadCmbSupName();
         loadCmbType();
         loadCmbSize();
-    }
-
-    public void printBtn(ActionEvent actionEvent) {
-    }
-
-    public void addBtn(ActionEvent actionEvent) {
-    }
-
-    public void clearBtn(ActionEvent actionEvent) {
-    }
-
-    public void saveBtn(ActionEvent actionEvent) {
-        Item item = new Item(
-                txtCode.getText(),
-                txtDesc.getText(),
-                Integer.parseInt(txtQty.getText()),
-                Double.parseDouble(txtSellPrice.getText()),
-                Double.parseDouble(txtBuyPrice.getText()),
-                cmbType.getValue().toString(),
-                cmbSize.getValue().toString(),
-                Double.parseDouble(txtProfit.getText().toString()),
-                cmbSupId.getValue().toString());
-
-        try {
-            boolean isAdded = CrudUtil.execute("INSERT INTO item VALUES(?,?,?,?,?,?,?,?,?)",
-                    item.getItemId(),
-                    item.getDesc(),
-                    item.getQty(),
-                    item.getSellingPrice(),
-                    item.getBuyingPrice(),
-                    item.getType(),
-                    item.getSize(),
-                    item.getProfit(),
-                    item.getSupplierId());
-            if(isAdded){
-                new Alert(Alert.AlertType.INFORMATION,"Item Saved..!").show();
-//                clearFields();
-//                loadTable();
-            }else{
-                new Alert(Alert.AlertType.ERROR,"Something went wrong..!").show();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public void loadCmbSupId(){
@@ -113,22 +65,7 @@ public class ItemController implements Initializable {
         }
     }
 
-    public void loadCmbSupName(){
-        try {
-            ResultSet resultSet = CrudUtil.execute("SELECT supplier_name FROM supplier");
-            ObservableList<String> supplierName = FXCollections.observableArrayList();
 
-            while(resultSet.next()){
-                supplierName.add(resultSet.getString(1));
-            }
-
-            cmbSupName.setItems(supplierName);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public void loadCmbType(){
         ObservableList<String> obs = FXCollections.observableArrayList("Male", "Female", "Kids");
@@ -138,5 +75,79 @@ public class ItemController implements Initializable {
     public void loadCmbSize(){
         ObservableList<String> obs = FXCollections.observableArrayList("Small", "Medium","Large");
         cmbSize.getItems().addAll(obs);
+    }
+
+    private void generateId() {
+        try {
+            ResultSet resultSet = CrudUtil.execute("SELECT item_id FROM item ORDER BY item_id DESC LIMIT 1");
+
+            if (resultSet.next()){
+                int num = Integer.parseInt(resultSet.getString(1).split("[S]")[1]);
+                num++;
+                txtCode.setText(String.format("S%03d",num));
+            }else {
+                txtCode.setText("S001");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printBtn(ActionEvent actionEvent) {
+    }
+
+    public void addBtn(ActionEvent actionEvent) {
+    }
+
+    public void clearBtn(ActionEvent actionEvent) {
+        clearFields();
+    }
+
+    public void saveBtn(ActionEvent actionEvent) {
+        double profit = Double.parseDouble(txtSellPrice.getText()) - Double.parseDouble(txtBuyPrice.getText());
+        Item item = new Item(
+                txtCode.getText(),
+                txtDesc.getText(),
+                Integer.parseInt(txtQty.getText()),
+                Double.parseDouble(txtSellPrice.getText()),
+                Double.parseDouble(txtBuyPrice.getText()),
+                cmbType.getValue().toString(),
+                cmbSize.getValue().toString(),
+                profit,
+                cmbSupId.getValue().toString());
+
+        try {
+            boolean isAdded = CrudUtil.execute("INSERT INTO item VALUES(?,?,?,?,?,?,?,?,?)",
+                    item.getItemId(),
+                    item.getDesc(),
+                    item.getQty(),
+                    item.getSellingPrice(),
+                    item.getBuyingPrice(),
+                    item.getType(),
+                    item.getSize(),
+                    item.getProfit(),
+                    item.getSupplierId());
+            if(isAdded){
+                new Alert(Alert.AlertType.INFORMATION,"Item Saved..!").show();
+                clearFields();
+//                loadTable();
+            }else{
+                new Alert(Alert.AlertType.ERROR,"Something went wrong..!").show();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void clearFields(){
+        generateId();
+        txtDesc.clear();
+        txtQty.clear();
+        txtBuyPrice.clear();
+        txtSellPrice.clear();
     }
 }
